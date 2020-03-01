473ce49dfdf1750ff50f3ee4f7594574d83f2de7
franco94 <francostavares2003@gmail.com>
2020-03-01 20:11:18 -0300

    botao para mudar sentido da conversao hectare e litros

--- app/src/main/java/com/example/conversordemedidas/MainActivity.java
+++ app/src/main/java/com/example/conversordemedidas/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,6 +19,11 @@ public class MainActivity extends AppCompatActivity {
     Spinner spinTipoConv;
     Spinner spinUnid;
     Spinner spinConv;
+    ImageButton imTroca;
+    EditText valorConverter;
+    TextView valorConvertido;
+
+    boolean orig;
 
     final String[] tipoConversao = new String[]{"Comprimento", "Área", "Volume", "Hectares", "Litros", "Massa", "Frequência", "Velocidade"};
 
@@ -76,6 +82,13 @@ public class MainActivity extends AppCompatActivity {
         spinTipoConv = (Spinner) findViewById(R.id.spinTipo);
         spinUnid = (Spinner) findViewById(R.id.spinUnid1);
         spinConv = (Spinner) findViewById(R.id.spinUnid2);
+        imTroca = (ImageButton) findViewById(R.id.imTrocaConv);
+
+        imTroca.setVisibility(View.INVISIBLE);
+        orig = true;
+
+        valorConverter = findViewById(R.id.editValor);
+        valorConvertido = findViewById(R.id.txtConversao);
 
         ArrayAdapter<String> adapterTipo = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, tipoConversao);
 
@@ -89,74 +102,114 @@ public class MainActivity extends AppCompatActivity {
 
                 if (position == 0) {
 
+                    imTroca.setVisibility(View.INVISIBLE);
+
                     final ArrayAdapter<String> adapterUnid = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, medidas1);
                     spinUnid.setAdapter(adapterUnid);
 
                     final ArrayAdapter<String> adapterConv = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, medidas2);
                     spinConv.setAdapter(adapterConv);
+
+                    valorConverter.setText("");
+                    valorConvertido.setText("");
                 }
 
                 if (position == 1){
+
+                    imTroca.setVisibility(View.INVISIBLE);
 
                     final ArrayAdapter<String> adapterUnid2 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, medidasquadradas1);
                     spinUnid.setAdapter(adapterUnid2);
 
                     final ArrayAdapter<String> adapterConv2 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, medidasquadradas2);
                     spinConv.setAdapter(adapterConv2);
+
+                    valorConverter.setText("");
+                    valorConvertido.setText("");
                 }
 
                 if(position == 2){
+
+                    imTroca.setVisibility(View.INVISIBLE);
 
                     final ArrayAdapter<String> adapterUnid3 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, medidascubicas1);
                     spinUnid.setAdapter(adapterUnid3);
 
                     final ArrayAdapter<String> adapterConv3 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, medidascubicas2);
                     spinConv.setAdapter(adapterConv3);
+
+                    valorConverter.setText("");
+                    valorConvertido.setText("");
                 }
 
                 if(position == 3){
+
+                    imTroca.setVisibility(View.VISIBLE);
 
                     final ArrayAdapter<String> adapterUnid4 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, medidashectares1);
                     spinUnid.setAdapter(adapterUnid4);
 
                     final ArrayAdapter<String> adapterConv4 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, medidashectares2);
                     spinConv.setAdapter(adapterConv4);
+
+                    valorConverter.setText("");
+                    valorConvertido.setText("");
                 }
 
                 if(position == 4){
+
+                    imTroca.setVisibility(View.VISIBLE);
 
                     final ArrayAdapter<String> adapterUnid5 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, medidascapacidade1);
                     spinUnid.setAdapter(adapterUnid5);
 
                     final ArrayAdapter<String> adapterConv5 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, medidascapacidade2);
                     spinConv.setAdapter(adapterConv5);
+
+                    valorConverter.setText("");
+                    valorConvertido.setText("");
                 }
 
                 if(position == 5){
+
+                    imTroca.setVisibility(View.INVISIBLE);
 
                     final ArrayAdapter<String> adapterUnid6 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, medidasmassa1);
                     spinUnid.setAdapter(adapterUnid6);
 
                     final ArrayAdapter<String> adapterConv6 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, medidasmassa2);
                     spinConv.setAdapter(adapterConv6);
+
+                    valorConverter.setText("");
+                    valorConvertido.setText("");
                 }
 
                 if(position == 6){
+
+                    imTroca.setVisibility(View.INVISIBLE);
 
                     final ArrayAdapter<String> adapterUnid7 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, medidasfrequencia1);
                     spinUnid.setAdapter(adapterUnid7);
 
                     final ArrayAdapter<String> adapterConv7 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, medidasfrequencia2);
                     spinConv.setAdapter(adapterConv7);
+
+                    valorConverter.setText("");
+                    valorConvertido.setText("");
                 }
 
                 if(position == 7){
+
+                    imTroca.setVisibility(View.INVISIBLE);
 
                     final ArrayAdapter<String> adapterUnid8 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, medidasvelocidade1);
                     spinUnid.setAdapter(adapterUnid8);
 
                     final ArrayAdapter<String> adapterConv8 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, medidasvelocidade2);
                     spinConv.setAdapter(adapterConv8);
+
+                    valorConverter.setText("");
+                    valorConvertido.setText("");
                 }
             }
 
@@ -169,8 +222,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickConversao(View v) {
 
-        EditText valorConverter = (EditText) findViewById(R.id.editValor);
-
         double valor;
         double convertido = 0.0;
 
@@ -181,7 +232,6 @@ public class MainActivity extends AppCompatActivity {
         } else {
 
             valor = parseDouble(valorConverter.getText().toString());
-            TextView valorConvertido = findViewById(R.id.txtConversao);
 
             int escolhaTipoConv = (int) (long) spinTipoConv.getSelectedItemId();
 
@@ -203,14 +253,33 @@ public class MainActivity extends AppCompatActivity {
 
                 case 3:
 
-                    base = 100;
-                    expoente = escolhaConv - (escolhaUnid - 5);
+                    if(orig) {
+
+                        base = 100;
+                        expoente = -(escolhaUnid - 5);
+                    }
+
+                    else{
+
+                        base = 100;
+                        expoente = escolhaConv - 5;
+                    }
                     break;
 
                 case 4:
 
-                    base = 1000;
-                    expoente = escolhaConv - (escolhaUnid - 2);
+                    if (orig) {
+
+                        base = 1000;
+                        expoente = - (escolhaUnid - 2);
+                    }
+
+                    else{
+
+                        base = 1000;
+                        expoente = escolhaConv - 2;
+                    }
+
                     break;
 
                 case 5:
@@ -230,6 +299,65 @@ public class MainActivity extends AppCompatActivity {
             convertido = valor / Math.pow(base, expoente);
             valorConvertido.setText(Double.toString(convertido) + " " + spinConv.getSelectedItem().toString());
         }
+    }
+
+    public void onClickTroca (View v){
+
+        int escolhaTipoConv = (int) (long) spinTipoConv.getSelectedItemId();
+        int escolhaUnid = (int) (long) spinUnid.getSelectedItemId();
+        int escolhaConv = (int) (long) spinConv.getSelectedItemId();
+
+        orig = !orig;
+
+        valorConverter.setText("");
+        valorConvertido.setText("");
+
+        if (orig) {
+
+            if(escolhaTipoConv == 3) {
+
+                final ArrayAdapter<String> adapterUnid4 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, medidashectares1);
+                spinUnid.setAdapter(adapterUnid4);
+
+                final ArrayAdapter<String> adapterConv4 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, medidashectares2);
+                spinConv.setAdapter(adapterConv4);
+
+            }
+
+            else if (escolhaTipoConv == 4){
+
+                final ArrayAdapter<String> adapterUnid5 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, medidascapacidade1);
+                spinUnid.setAdapter(adapterUnid5);
+
+                final ArrayAdapter<String> adapterConv5 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, medidascapacidade2);
+                spinConv.setAdapter(adapterConv5);
+            }
+        }
+
+        else{
+
+            if(escolhaTipoConv == 3) {
+
+                final ArrayAdapter<String> adapterUnid4 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, medidashectares1);
+                spinConv.setAdapter(adapterUnid4);
+
+                final ArrayAdapter<String> adapterConv4 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, medidashectares2);
+                spinUnid.setAdapter(adapterConv4);
+            }
+
+            else if (escolhaTipoConv == 4){
+
+                final ArrayAdapter<String> adapterUnid5 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, medidascapacidade1);
+                spinConv.setAdapter(adapterUnid5);
+
+                final ArrayAdapter<String> adapterConv5 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, medidascapacidade2);
+                spinUnid.setAdapter(adapterConv5);
+
+//                escolhaUnid = escolhaConv;
+            }
+        }
+
+
     }
 }
 
